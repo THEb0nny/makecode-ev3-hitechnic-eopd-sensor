4dd7f74e7bd033463c033b4b675d01bcce5cbac8
Dmitriy Antipov <aston_bonny@mail.ru>
2024-10-22 22:34:11 +0300

    update

--- main.ts
+++
@@ -1,7 +0,0 @@
-forever(function () {
-    brick.clearScreen()
-    brick.printValue("nls1", sensors.nxtLight1.light(NXTLightIntensityMode.ReflectedRaw), 1)
-    brick.printValue("eopd2", sensors.htEOPD2._readPin1(), 2)
-    brick.printValue("nls4", sensors.nxtLight4.light(NXTLightIntensityMode.Reflected), 3)
-    pause(10)
-})
